6ae08ea89967f4ee0cefc14900487846447c539d
Jon Wingfield <wingfield.jon@gmail.com>
2013-06-22 20:08:31 -0400

    Fixing most whitespace issues (still have one when two sections are back to back (<#test>\n</test>\n doesn't work)

--- Nustache.Core/Scanner.cs
+++ Nustache.Core/Scanner.cs
@@ -1,67 +1,74 @@
 ﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.IO;
+using System.Text;
 
 namespace Nustache.Core
 {
     public class Scanner
     {
         private static readonly Regex _markerRegex = new Regex(@"\{\{([\{]?[^}]+?\}?)\}\}");
-        private static readonly Regex _stripRegex = new Regex(@"\G[\r\t\v ]+\n");
+        private static readonly Regex _standaloneRegex = new Regex(@"(^|\r?\n)[\r\t\v ]*({\{\s*[#\/!\<\>^]+[^}]+?\}\})[\r\t\v ]*(\r?\n|$)");
 
         public IEnumerable<Part> Scan(string template)
         {
             int i = 0;
             Match m;
 
-            while ((m = _markerRegex.Match(template, i)).Success)
+            template = _standaloneRegex.Replace(template, match => match.Groups[1].Value + match.Groups[2].Value);
+            
+            while (true)
             {
-                string literal = template.Substring(i, m.Index - i);
+                if ((m = _markerRegex.Match(template, i)).Success)
+                {
+                    string literal = template.Substring(i, m.Index - i);
 
-                if (literal != "")
-                {
-                    yield return new LiteralText(literal);
-                }
+                    if (literal != "")
+                    {
+                        yield return new LiteralText(literal);
+                    }
 
-                string marker = m.Groups[1].Value;
-                bool stripOutNewLine = false;
+                    string marker = m.Groups[1].Value;
 
-                marker = marker.Trim();
+                    marker = marker.Trim();
 
-                if (marker[0] == '#')
-                {
-                    yield return new Block(marker.Substring(1));
-                    stripOutNewLine = true;
-                } 
-                else if (marker[0] == '^') 
-                {
-                    yield return new InvertedBlock(marker.Substring(1));
-                    stripOutNewLine = true;
-                }
-                else if (marker[0] == '<')
-                {
-                    yield return new TemplateDefinition(marker.Substring(1));
-                    stripOutNewLine = true;
-                }
-                else if (marker[0] == '/')
-                {
-                    yield return new EndSection(marker.Substring(1));
-                    stripOutNewLine = true;
-                }
-                else if (marker[0] == '>')
-                {
-                    yield return new TemplateInclude(marker.Substring(1));
-                    stripOutNewLine = true;
-                }
-                else if (marker[0] != '!')
-                {
-                    yield return new VariableReference(marker.Trim());
-                }
+                    if (marker[0] == '#')
+                    {
+                        yield return new Block(marker.Substring(1).Trim());
+                    }
+                    else if (marker[0] == '^')
+                    {
+                        yield return new InvertedBlock(marker.Substring(1).Trim());
+                    }
+                    else if (marker[0] == '<')
+                    {
+                        yield return new TemplateDefinition(marker.Substring(1).Trim());
+                    }
+                    else if (marker[0] == '/')
+                    {
+                        yield return new EndSection(marker.Substring(1).Trim());
+                    }
+                    else if (marker[0] == '>')
+                    {
+                        yield return new TemplateInclude(marker.Substring(1).Trim());
+                    }
+                    else if (marker[0] != '!')
+                    {
+                        yield return new VariableReference(marker.Trim());
+                    }
 
-                i = m.Index + m.Length;
+                    i = m.Index + m.Length;
 
-                Match s;
-                if (stripOutNewLine && (s = _stripRegex.Match(template, i)).Success)
-                    i += s.Length;
+                    //Match s;
+                    //if (standalone && (s = _stripRegex.Match(template, i)).Success)
+                    //    i += s.Length;
+                    //if (stripOutNewLine && template[i] == '\n')
+                    //    i += 1;
+                }
+                else
+                {
+                    break;
+                }
             }
 
             if (i < template.Length)
